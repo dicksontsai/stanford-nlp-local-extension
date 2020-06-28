e44b2098e1efb1405aa7fbd7dd797bab0c691483
Dickson Tsai <dickson.tsai@berkeley.edu>
2020-06-28 10:50:05 -0700

    Display pos tags better

--- src/js/parseWindow/App.jsx
+++ src/js/parseWindow/App.jsx
@@ -1,5 +1,53 @@
 import React from "react";
 
+// Taken from https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+const posTagToNameMap = {
+  CC: "Coordinating conjunction",
+  CD: "Cardinal number",
+  DT: "Determiner",
+  EX: "Existential there",
+  FW: "Foreign word",
+  IN: "Preposition or subordinating conjunction",
+  JJ: "Adjective",
+  JJR: "Adjective, comparative",
+  JJS: "Adjective, superlative",
+  LS: "List item marker",
+  MD: "Modal",
+  NN: "Noun, singular or mass",
+  NNS: "Noun, plural",
+  NNP: "Proper noun, singular",
+  NNPS: "Proper noun, plural",
+  PDT: "Predeterminer",
+  POS: "Possessive ending",
+  PRP: "Personal pronoun",
+  PRP$: "Possessive pronoun",
+  RB: "Adverb",
+  RBR: "Adverb, comparative",
+  RBS: "Adverb, superlative",
+  RP: "Particle",
+  SYM: "Symbol",
+  TO: "to",
+  UH: "Interjection",
+  VB: "Verb, base form",
+  VBD: "Verb, past tense",
+  VBG: "Verb, gerund or present participle",
+  VBN: "Verb, past participle",
+  VBP: "Verb, non-3rd person singular present",
+  VBZ: "Verb, 3rd person singular present",
+  WDT: "Wh-determiner",
+  WP: "Wh-pronoun",
+  WP$: "Possessive wh-pronoun",
+  WRB: "Wh-adverb"
+};
+
+function posTagToName(tag) {
+  return posTagToNameMap[tag] || tag;
+}
+
+function displayPosTag(tag) {
+  return `${tag} (${posTagToName(tag)})`;
+}
+
 // countsToOrderedList converts a JSON object of counts into an array of
 // entries sorted by decreasing order.
 // Example: {"foo": 5, "bar": 10} -> [["bar", 10], ["foo", 5]]
@@ -17,42 +65,50 @@ function PosTable(props) {
       return a[0].localeCompare(b[0]);
     });
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Part of Speech</th>
-          <th>Word counts</th>
-        </tr>
-      </thead>
-      <tbody>
-        {sorted.map(entry => (
-          <tr key={entry[0]}>
-            <td>{entry[0]}</td>
-            <td>
-              <pre>{entry[1].join("\n")}</pre>
-            </td>
+    <div>
+      <h1>Word Choice by Part of Speech</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Part of Speech</th>
+            <th>Word counts</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sorted.map(entry => (
+            <tr key={entry[0]}>
+              <td>{displayPosTag(entry[0])}</td>
+              <td>
+                <pre>
+                  {entry[1].map(wordCount => wordCount.join(": ")).join("\n")}
+                </pre>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
 function TreesTable(props) {
   return (
-    <table>
-      <tbody>
-        {props.trees.map((tree, i) => {
-          return (
-            <tr key={`tree-${i}`}>
-              <td>
-                <div className="tree">{tree.replace(/\\n/g, "\n")}</div>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <h1>Parse Trees</h1>
+      <table>
+        <tbody>
+          {props.trees.map((tree, i) => {
+            return (
+              <tr key={`tree-${i}`}>
+                <td>
+                  <div className="tree">{tree.replace(/\\n/g, "\n")}</div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
